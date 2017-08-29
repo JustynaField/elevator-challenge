104d75c10b599e27d56302d4ee095182e559fe07
Justyna Field <justyna.field@gmail.com>
2017-08-28 19:49:57 -0600

    define elevator class

--- elevator.js
+++ elevator.js
@@ -1,5 +1,37 @@
 export default class Elevator {
   constructor() {
-    
+    this.startingFloor = 0;
+    this.currentFloor = null;
+    this.targetFloors = [];
+    this.floorRequests = [];
+    this.passengerNumber = [];
   }
+}
+
+Elevator.prototype.reset = () => {
+
+}
+
+
+Elevator.prototype.goToFloor = (person) => {
+  this.passengetNumber.push(person)
+  this.targetFloors.push()
+}
+
+
+//pickUpPerson
+//on their current floor
+//drop off on reqiested floor
+
+
+export class Person {
+  constructor () {
+    this.name = ''
+    this.currentFloor = null;
+    this.dropOffFloor = null;
+  }
+
+
+
+
 }
